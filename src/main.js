c29ca1cabf87232e22c2f1a00f8c72f715c78be8
vakihun <vakihun68@gmail.com>
2020-12-05 11:23:27 +0100

    error handling created

--- src/main.js
+++ src/main.js
@@ -1,42 +1,38 @@
 const readline = require("readline");
 const translate = require('@vitalets/google-translate-api');
-let before = 'hu';
-const alllanguages = ['af','sq','ar','hy','az','eu','be','bg','ca','zh-CN','hr','cs','da','nl','en','et','tl','fi','fr','gl','ka','hi','iw','sv','sl','ru','pl','no','mk','lt','ja','ga']
+let beforeUsedLanguage = 'hu';
+const allLanguages = ['af','sq','ar','hy','az','eu','be','bg','ca','zh-CN','hr','cs','da','nl','en','et','tl','fi','fr','gl','ka','hi','iw','sv','sl','ru','pl','no','mk','lt','ja','ga']
 
-const rl = readline.createInterface({
+const userTextIO = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question("Írja be a szöveget:", function(text) {
-    rl.question("Írja be a fordítások számát:", function(num) {
-
-        const langarray = [];
-        for(var i = 0;i<num;i++)
+userTextIO.question("Írja be a szöveget:", function(textToTranslate) {
+    userTextIO.question("Írja be a fordítások számát:", function(numberOfTranslates) {
+        const allUsedLangs = [];
+        for(var i = 0;i<numberOfTranslates;i++)
         {
-            var item = alllanguages[Math.floor(Math.random() * alllanguages.length)];
-            while(langarray.includes(item))
-            {
-                item = alllanguages[Math.floor(Math.random() * alllanguages.length)];
-            }
-            langarray.push(item);
+            var item = allLanguages[Math.floor(Math.random() * allLanguages.length)];
+            allUsedLangs.push(item);
         }
-        langarray.push('hu');
-        async function translattext() {
-            for (const i of langarray) {
-                await translate(text, {from: before, to: i}).then(res => {
-                    //console.log(res);
-                    if(i=='hu') console.log();
-                    console.log('['+before + ']-[' + i+']:'+res.text);
-                    text=res.text;
-                    before = i;
-                }).catch(err => {
-                    console.error(err);
-                });
-            }
-        }
+        allUsedLangs.push('hu');
 
-        translattext();
+            async function translateText() {
+                for (const currentLang of allUsedLangs) {
+                        let foundAnError = false;
+                        await translate(textToTranslate, {from: beforeUsedLanguage, to: currentLang}).then(res => {
+                            if(currentLang=='hu') console.log();
+                            console.log('['+beforeUsedLanguage + ']-[' + currentLang+']:'+res.text);
+                            textToTranslate=res.text;
+                            beforeUsedLanguage = currentLang;
+                        }).catch(err => {
+                            console.log("Hiba történt a szerverhez való kapcsolódás során: "+err.statusCode+" "+ err.statusMessage);
+                            foundAnError = true;
+                        });
+                        if(foundAnError) break;
+                }
+            }
+        translateText();
     });
 });
-
